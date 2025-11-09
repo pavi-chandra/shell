c02c62276afdccbcee82ec897bd427256bac44d9
pavi-chandra <238516727+pavi-chandra@users.noreply.github.com>
2025-11-08 22:24:27 -0500

    Attempt 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -42,24 +42,24 @@ cd data
 mkdir processed/server_logs processed/user_logs processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/raw
-cp *server*.log ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/server_logs
+cd cd raw
+cp *server*.log ../data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp *user* ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/user_logs
-cp *event* ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/event_logs
+cp *user*.log ../data/processed/user_logs
+cp *event*.log ../data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm *ipaddr* 
-cd ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/user_logs
+cd ../data/processed/user_logs
 rm *ipaddr*
-cd ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/event_logs 
+cd ../data/processed/event_logs 
 rm *ipaddr*
-cd ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/processed/server_logs
+cd ../data/processed/server_logs
 rm *ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd ~/Desktop/dsi3/shell/02_activities/assignments/newproject/data/
+cd data
 ls processed > inventory.txt
 
 ###########################################
